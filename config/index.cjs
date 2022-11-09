a879d63884f64a83a7cec662442d608b691d3d28
weixin9967 <117797009+weixin9967@users.noreply.github.com>
2022-11-09 13:31:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小李',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oRlv556AwFGcqRYVWPgAJIcM21C4',
+      id: 'oRlv55zTQvtEpEJiKv8sXrk_F18E',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Q6qnvl6OLMc9zqHIAGmYPQvT2pyiXm5qSckKj7fpBM0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -42,7 +42,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0H1L0JA_T9DmkM7lHyO4Okqw_8ClSoE_J9YH_LHREnc	',
+  CALLBACK_TEMPLATE_ID: '0H1L0JA_T9DmkM7lHyO4Okqw_8ClSoE_J9YH_LHREnc',
 
   CALLBACK_USERS: [
     {
