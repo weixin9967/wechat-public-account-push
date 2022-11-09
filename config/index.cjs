4b984a0fc98cc623f1b6dbabd9b35efcd3ce094b
weixin9967 <117797009+weixin9967@users.noreply.github.com>
2022-11-09 13:23:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,31 +27,22 @@ const USER_CONFIG = {
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-12',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-      //  {
-      //    type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-      //  },
+       
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '小李', year: '1999', date: '10-12',
         },
-       // {
-      //    type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-       // },
+       {
+         type: '节日', name: '相识纪念日', year: '2022', date: '10-11',
+        },
       ],
-      // 我们在一起已经有xxxx天了的配置
-     // customizedDateList: [
-        // 在一起的日子
-      //  { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-      //  { keyword: 'marry_day', date: '2022-09-09' },
-    //  ],
+     
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	0H1L0JA_T9DmkM7lHyO4Okqw_8ClSoE_J9YH_LHREnc',
+  CALLBACK_TEMPLATE_ID: '0H1L0JA_T9DmkM7lHyO4Okqw_8ClSoE_J9YH_LHREnc	',
 
   CALLBACK_USERS: [
     {
